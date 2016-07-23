525d308895cbd549967bc6b7c2b52e7575108c10
Tonghe Wang <hetong583@qq.com>
2016-07-23 13:57:34 +0800

    change scene

--- Assets/Script/Menu.cs
+++ Assets/Script/Menu.cs
@@ -1,23 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.IO;
 
 public class Menu : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
-	}
-
-	public void loadSong1() {
-		Debug.Log ("loading song 1");
-	}
-
-	public void loadSong2() {
-		Debug.Log ("loading song 2");
 	}
 }
